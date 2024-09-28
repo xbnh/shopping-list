ccd7e877a143c4cff99c3f913aa5660d2793ede3
Hesham Elhamdani <helhamdani@cnm.edu>
2024-09-28 02:29:35 -0600

    Commiting Update

---
+++ script.js
@@ -0,0 +1,64 @@
+window.onload = function() {
+    initShoppingList();
+};
+
+function initShoppingList() {
+    let form = document.getElementById("item-form");
+
+    form.addEventListener("submit", (event) => {
+         handleItemForm(event, form);
+    });
+}
+
+function handleItemForm(event, formRef) {
+    if(event.preventDefault) {
+        event.preventDefault();
+    }
+
+    addItemToShoppingList();
+    formRef.reset();
+
+    return false;
+}
+
+function addItemToShoppingList() {
+    let itemName = document.getElementById("item-name");
+    let itemAmount = document.getElementById("item-amount");
+    let id = getRandomInt(0, 10000000);
+
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
+    let itemListRef = document.getElementById("shopping-list");
+    itemListRef.insertAdjacentHTML("afterend", itemHtml);
+
+    setDeleteButtonEvent(id);
+}
+function setDeleteButtonEvent(id) {
+    let deleteButton = document.getElementById("button-" + id);
+    deleteButton.addEventListener("click", () => {
+        removeListItem(id);
+    });
+}
+
+
+    function createListItemHtml(itemName, itemAmount, id) {
+     return `
+        <li id="item-${id}">
+            ${itemName} - ${itemAmount}
+            <button type="button" id="button-${id}">Delete Item</button>
+        </li>
+    `;
+}
+function removeListItem(id) {
+    let listItem = document.getElementById("item-" + id);
+    if (listItem) {
+        listItem.parentNode.removeChild(listItem);
+    } else {
+        console.error("List item not found for id:", id);
+    }
+}
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+}
